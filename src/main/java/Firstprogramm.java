df44bf051b8e2cb834392323d2e70f8bae45ca1f
Radikus <dr.alchimik@yandex.ru>
2021-11-04 18:02:31 +0500

    My second

--- src/main/java/Firstprogramm.java
+++ src/main/java/Firstprogramm.java
@@ -1,6 +1,7 @@
 public class Firstprogramm {
 
     public static void main(String[] args) {
-        System.out.println("Hello World123!");
+            System.out.println("Hello World123!");
+
     }
 }
