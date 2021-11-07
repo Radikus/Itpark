bf609644d959bb87b2fd933ea517c5d86c10c7e5
Radikus <dr.alchimik@yandex.ru>
2021-11-07 10:37:32 +0500

    My second

--- src/main/java/SecondLesson.java
+++ src/main/java/SecondLesson.java
@@ -1,26 +1,26 @@
 public class SecondLesson {
 
     public static void main(String[] args) {
-
+        //сложение
         int arg1=10;
         int arg2=5;
         int arg3 = arg1 + arg2;
         System.out.println(arg3);
-         //сложение
+        //умножение
         int arg11=10;
         int arg21=5;
         int arg31 = arg11 * arg21;
         System.out.println(arg31);
-         //умножение
+        //деление
         int arg6=10;
         int arg5=5;
         int arg4 = arg6 / arg5;
         System.out.println(arg4);
-         //деление
+        //вычитание
         int arg16=10;
         int arg15=5;
         int arg14 = arg16 - arg15;
         System.out.println(arg14);
-        //вычитание
+
     }
 }
